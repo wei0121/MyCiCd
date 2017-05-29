32bf5cde8daa98d232fbfc3f1eddafb791506a37
Weiren <williamtse0121@gmail.com>
2017-05-29 08:42:32 +0200

    init

---
+++ launch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# docker build -t my-ruby-app ./docker-app
+# docker build -t my-nginx ./docker-nginx
+# docker run --name ruby-app -p 4567:4567 -d my-ruby-app
+# docker run --name nginx-container \
+#   -v $(pwd)/html:/usr/share/nginx/html:ro \
+#   -v $(pwd)/docker-nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
+#   --link ruby-app:app \
+#   -P -d my-nginx
+# curl http://$(docker-machine ip dev):32769/
+# curl http://$(docker-machine ip dev):32769/test.html
+# curl http://$(docker-machine ip dev):32769/app/
+# curl http://$(docker-machine ip dev):32769/app/foo
+
+docker build -t memo-nginx ./nginx
+# docker build -t memo-myHelloWorld ./myHelloWorld
+# docker build -t memo-registry ./registry
+# docker build -t memo-jenkins ./jenkins
+
+docker run -it -p 80:8080  -v `pwd`/logs:/var/log/nginx dockerfile/nginx
+
+# docker run --name memo-nginx -p 80:8080 -d memo-nginx
+# docker run --name memo-myHelloWorld -p 80:8080 -d memo-nginx
+# docker run --name memo-registry -p 4567:4567 -d memo-registry
+# docker run -d -p 49001:8080 -v $PWD/jenkins:/var/jenkins_home:z -t jenkins
