774c7ad0fedff29c85b5bc915021e6961461b168
Weiren Xie <weirenxie@weirens-MBP.inventorum>
2017-06-07 15:04:47 +0200

    build

--- dsm_docker_launch.sh
+++ dsm_docker_launch.sh
@@ -8,14 +8,18 @@ chown 1000:1000 /volume1/docker/container_data/logs/jenkins
 chown 1000:1000 /volume1/docker/container_data/logs/nginx
 chown 1000:1000 /volume1/docker/container_data/registry
 
+docker build -t memo-data ./data
+docker build -t memo-myhelloworld ./myHelloWorld
+docker build -t memo-jenkins ./jenkins
+docker build -t memo-nginx ./nginx
+docker build -t memo-registry ./registry
+
 docker run -d \
   -v /volume1/docker/container_data/jenkins_home:/var/jenkins_home \
   -v /volume1/docker/container_data/logs/jenkins:/var/log/jenkins \
   -v /volume1/docker/container_data/logs/nginx:/var/log/nginx \
   -v /volume1/docker/container_data/registry:/var/lib/registry \
   --name memo-data memo-data
-
-  # -v `pwd`/registry/config.yml:/etc/docker/registry/config.yml \
 
 docker run -d --name memo-myhelloworld memo-myhelloworld
 
